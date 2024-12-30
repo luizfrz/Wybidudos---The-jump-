683dd88afe07114371141954fe0ccb9c45916a33
luiz frança <kazumi200516@gmail.com>
2024-12-30 11:57:35 -0300

    score

--- Wibydudos -  The jump/Js/script.js
+++ Wibydudos -  The jump/Js/script.js
@@ -1,8 +1,7 @@
-// EXPLICANDO CODIGO JAVASCRIPT
-// criação de variaveis selecionando elemento no index.html
 const bigodinho = document.querySelector('.bigodinho');
 const maleta = document.querySelector('.maleta');
-// vai definir uma função chamada 'jump' que adiciona uma classe e remove uma classe no CSS 'jump com intervalo de 500 ms 
+const recorde = document.querySelector('.recorde')
+let record = 0;
 const jump = () => {
      bigodinho.classList.add('jump');
      
@@ -14,28 +13,24 @@ const loop = setInterval (() =>{
 
    const maletaPosition = maleta.offsetLeft;
    const bigodinhoPosition = +window.getComputedStyle(bigodinho).bottom.replace('px', '');
-// vai fazer verificação se maleta ou bigodinho estão colidindo
    if (maletaPosition < 80 && maletaPosition > 0 && bigodinhoPosition < 80){
-      
-      // vai fazer animação 'maleta' e define uma posição horizontal
+
       maleta.style.animation = 'none';
       maleta.style.left = `${maletaPosition}px`;
-
-      
       maleta.style.animation = 'none';
       maleta.style.bottom = `${bigodinhoPosition}px`;
 
-      // vai alterar a imagem 'bigodinho' e ajusta  a largura e a margem da esquerda
       bigodinho.src = 'sprite/jubydudos.png';
       bigodinho.style.width = '130px'
       bigodinho.marginLeft = '30px'
-      // limpa o intervalo para interromper a execução e continuar o loop 
-      clearInterval(loop);
 
+      clearInterval(loop);
+      alert(`Seu recorde foi de: ${record}`);
+   }else if(maletaPosition <0){
+      record ++;
+      recorde.textContent = `seu ponto foi de: ${record}`
    }
 } ,10);
 
-// clicando na tela vai fazer personagem pular 
 document.addEventListener('click',jump);
-// apertando em qualquer tecla vai fazer personagem pular 
 document.addEventListener('keydown', jump); 
